0e1936d3eeb49f924dd57e824306c8b490e34097
aaron <aaron0lovegrove@gmail.com>
2021-10-29 15:04:39 +0000

    all api endpoints tested

--- api/core/routes.py
+++ api/core/routes.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, Body, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-
+import json
+from bson import json_util, ObjectId
 from .models import ArticleSchema
 
+
 router = APIRouter()
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 
 
 @router.post("/", response_description="Add new article")
@@ -12,7 +18,7 @@ async def create_task(request: Request, article: ArticleSchema = Body(...)):
     article = jsonable_encoder(article)
     new_article = await request.app.mongodb["articles"].insert_one(article)
     created_article = await request.app.mongodb["articles"].find_one(
-        {"_id": str(new_article.inserted_id)}
+        {"_id": ObjectId(new_article.inserted_id)}
     )
 
     return JSONResponse(status_code=status.HTTP_201_CREATED)
@@ -22,15 +28,15 @@ async def create_task(request: Request, article: ArticleSchema = Body(...)):
 async def list_articles(request: Request):
     articles = []
     for a in await request.app.mongodb["articles"].find().to_list(length=250):
-        articles.append(a)
+        articles.append(parse_json(a))
     return articles
 
 
 @router.get("/{id}", response_description="Get a single article")
 async def show_article(id: str, request: Request):
     if (
-        article := await request.app.mongodb["articles"].find_one({"_id": id})
+        article := await request.app.mongodb["articles"].find_one({"_id": ObjectId(id)})
     ) is not None:
-        return article
+        return parse_json(article)
 
     raise HTTPException(status_code=404, detail=f"Article {id} not found")
