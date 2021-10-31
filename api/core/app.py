48db5cb1b5642bcc07077eb0759d9009155caada
aaron <aaron0lovegrove@gmail.com>
2021-10-31 21:39:17 +0000

    category pages working

--- api/core/app.py
+++ api/core/app.py
@@ -72,3 +72,19 @@ async def get_article(id: str, request: Request):
         )
 
     raise HTTPException(status_code=404, detail=f"Article {id} not found")
+
+
+@app.get("/{cat}", response_class=HTMLResponse)
+async def get_pub_home(cat: str, request: Request):
+    articles = []
+    for a in (
+        await request.app.mongodb["articles"]
+        .find({"category": str.capitalize(cat)})
+        .sort("pubdate", -1)
+        .to_list(length=100)
+    ):
+        articles.append(a)
+
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "articles": articles}
+    )
