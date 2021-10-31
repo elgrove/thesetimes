0c1f2a5614960bff399ee6fcaff9834f14e62781
aaron <aaron0lovegrove@gmail.com>
2021-10-31 13:15:43 +0000

    add first run check

--- scraper/main.py
+++ scraper/main.py
@@ -1,7 +1,9 @@
+from requests import api
 import schedule
 import time
 from utils.update import update_db
 from utils.update import api_url
+import requests
 
 schedule.every().day.at("07:00").do(update_db)
 schedule.every().day.at("12:00").do(update_db)
@@ -10,7 +12,12 @@ schedule.every().day.at("19:00").do(update_db)
 
 
 if __name__ == "__main__":
-    # update_db()
+    time.sleep(5)  # wait for api to come online
+    r = requests.get(api_url)
+    # check if db is empty, if so do first run
+    if len(r.json()) == 0:
+        update_db()
+
     while True:
         schedule.run_pending()
         time.sleep(1)
