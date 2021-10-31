a874878ecb5db2f698cf1d9f581fe4d343767044
aaron <aaron0lovegrove@gmail.com>
2021-10-31 12:31:49 +0000

    scheduled update times 07, 12, 16, 19

--- scraper/main.py
+++ scraper/main.py
@@ -2,10 +2,15 @@ import schedule
 import time
 from utils.update import update_db
 
-schedule.every(6).hours.do(update_db)
+
+schedule.every().day.at("07:00").do(update_db)
+schedule.every().day.at("12:00").do(update_db)
+schedule.every().day.at("16:00").do(update_db)
+schedule.every().day.at("19:00").do(update_db)
+
 
 if __name__ == "__main__":
-    update_db()
+    # update_db()
     while True:
         schedule.run_pending()
         time.sleep(1)
