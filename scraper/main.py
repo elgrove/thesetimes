c18b7b1cd640bf32c7347743565665c6e50a9536
aaron <aaron0lovegrove@gmail.com>
2021-11-10 15:20:04 +0000

    bits

--- scraper/main.py
+++ scraper/main.py
@@ -9,13 +9,13 @@ import requests
 # schedule.every().day.at("12:00").do(update_db)
 # schedule.every().day.at("17:00").do(update_db)
 
-schedule.every(2).hours.at(":55").do(update_db)
+schedule.every().hour.do(update_db)
 
 if __name__ == "__main__":
     time.sleep(4)  # wait for api to come online
 
-    # testing
-    update_db()
+    # # testing
+    # update_db()
 
     r = requests.get(api_url)
     # check if db is empty, if so do first run
