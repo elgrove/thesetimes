4964d2a6b658451d2e9b4554ba446f27ffed4003
Aaron <aaron0lovegrove@gmail.com>
2023-03-18 21:38:46 +0000

    fix: wip - i dont like django

--- api/thesetimes/settings.py
+++ api/thesetimes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,14 @@ WSGI_APPLICATION = "thesetimes.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["DATABASE_SERVICE"],
+        "PORT": os.environ["POSTGRES_PORT"],
+    }
 }
 
 
