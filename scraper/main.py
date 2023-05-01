00e40b1930d238781b69742cb428ce629771e2f6
alvgrv <aaron.lovegrove@essentia-analytics.com>
2023-05-01 22:48:53 +0100

    feat: scheduler

--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,20 @@
+import os
+from time import sleep
+
+import schedule
+
 from core.director import ScraperDirector
+
+def main():
+    """Entry point for the scraper container. Runs on startup then every n hours as defined by an env var."""
+    scraper = ScraperDirector()
+    scraper.run()
+    scrape_interval = int(os.environ["SCRAPE_INTERVAL_HR"])
+    schedule.every(scrape_interval).hours.do(scraper.run)
+    while True:
+        schedule.run_pending()
+        sleep(1)
 
 
 if __name__ == "__main__":
-    scraper_director = ScraperDirector()
-    scraper_director.run()
+    main()
